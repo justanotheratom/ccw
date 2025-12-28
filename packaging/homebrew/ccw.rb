6d3d0de16c59dbc3b80b4e4411d95f50352a9e1c
justanotheratom <sanketpatel.1805@gmail.com>
2025-12-28 23:44:37 +0530

    Tag v0.1.0 and update Homebrew formula

--- packaging/homebrew/ccw.rb
+++ packaging/homebrew/ccw.rb
@@ -3,8 +3,8 @@ class Ccw < Formula
   homepage "https://github.com/ccw/ccw"
   version "0.1.0"
 
-  url "https://github.com/ccw/ccw/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "ADD_SHA256_HERE"
+  url "https://github.com/justanotheratom/ccw/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "a2cf5f47b87ed51615bc544f8e2403e05d4203a66046bd1800144f7b5f5fa8c7"
   license "MIT"
   head "https://github.com/ccw/ccw.git", branch: "main"
 
