dcfe0031cc28d8ed2443a9c783e0ef978769c6fe
justanotheratom <sanketpatel.1805@gmail.com>
2025-12-30 08:13:55 +0530

    Automate tap cask updates

--- packaging/homebrew/ccw-menubar.rb
+++ packaging/homebrew/ccw-menubar.rb
@@ -2,7 +2,7 @@ cask "ccw-menubar" do
   version "1.0.0"
   sha256 "..."
 
-  url "https://github.com/justanotheratom/ccw/releases/download/v#{version}/CCWMenubar.dmg"
+  url "https://github.com/justanotheratom/ccw/releases/download/menubar-v#{version}/CCWMenubar.dmg"
   name "CCW Menubar"
   desc "Menubar app for Claude Code Workspace manager"
   homepage "https://github.com/justanotheratom/ccw"
